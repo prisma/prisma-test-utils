d30ed75982008c65137c5f4901cf46a242cb2cae
maticzav <matic.zavadlal@gmail.com>
2019-06-12 11:39:50 +0200

    separate the debugger file from example test files

---
+++ examples/simple-prisma/__tests__/debug.ts
@@ -0,0 +1,11 @@
+import { seed } from '../../../packages/prisma-faker/src'
+import * as photon from '@generated/photon'
+
+debugger
+;(seed(photon, bag => ({})) as Promise<object[]>)
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    throw err
+  })
