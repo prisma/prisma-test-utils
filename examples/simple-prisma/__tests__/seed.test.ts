eb2aeb5f0f0a2e9da01c9505c41e7b3ebba96756
maticzav <matic.zavadlal@gmail.com>
2019-06-12 09:43:05 +0200

    example

---
+++ examples/simple-prisma/__tests__/seed.test.ts
@@ -0,0 +1,16 @@
+import { seed } from '@prisma/faker'
+import photon from '@generated/photon'
+
+beforeAll(async () => {
+  debugger
+  const data = await seed(photon, bag => ({}))
+
+  console.log(data)
+})
+
+test('user is queried correctly', async () => {
+  const client = new photon.Photon()
+  const posts = await client.blogs()
+
+  expect(posts.length).toBe(5)
+})
